e84ca6dcadeebc18c02ee7c2f37261c2295e9f2d
shaolianfk <871089057@qq.com>
2021-06-28 14:06:47 +0800

    [fix] update monitor

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './styles/index.scss';
 import App from './App';
-import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
 
-const tarckBase = 'http://121.199.23.187:86'
-
-const instance = createInstance({
-  urlBase: tarckBase,
-  siteId: 1,
-  heartBeat: { // optional, enabled by default
-    active: true, // optional, default value: true
-    seconds: 10 // optional, default value: `15
-  },
-  configurations: { // optional, default value: {}
-    // any valid matomo configuration, all below are optional
-    setRequestMethod: 'POST'
-  },
-})
 ReactDom.render(
-  <MatomoProvider value={instance}>
-    <App />
-  </MatomoProvider>,
+  <App />,
   document.getElementById('shaolian')
 )
