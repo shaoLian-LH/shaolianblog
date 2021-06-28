1c4fe77ff09083866f35a5ed9d9066b05114465c
shaolianfk <871089057@qq.com>
2021-06-28 14:36:34 +0800

    [feature] 更新monitor

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,26 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './styles/index.scss';
 import App from './App';
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react'
+
+const instance = createInstance({
+  urlBase: 'http://121.199.23.187:86/',
+  siteId: 1,
+  heartBeat: { // optional, enabled by default
+    active: true, // optional, default value: true
+    seconds: 10 // optional, default value: `15
+  },
+  configurations: { // optional, default value: {}
+    // any valid matomo configuration, all below are optional
+    disableCookies: false,
+    setSecureCookie: false,
+    setRequestMethod: 'POST'
+  }
+})
 
 ReactDom.render(
-  <App />,
+  <MatomoProvider value={instance}>
+    <App />
+  </MatomoProvider>,
   document.getElementById('shaolian')
 )
